a8be35ebed1202f0e83eb9e327e21a5044823b8a
Jonathan Richmond <richmo11@purdue.edu>
2025-06-05 01:24:56 -0400

    Created EMPlanarOrbit.m

---
+++ EMPlanarOrbit.m
@@ -0,0 +1,47 @@
+%%% Plot BCR4BP Trajectory
+%%% Jonathan Richmond
+%%% C: 4 June 2025
+
+clear
+load('../PhDScripts/Output/EMPlanarOrbit.mat')
+
+%% Earth-Moon Data
+gmE = 3.9860043543609593E5; % Earth gravitational parameter [km^3/s^2]
+mE = gmE/6.67384E-20; % Earth mass [kg]
+RE = 6.3710083666666660E3; % Earth radius [km]
+
+gmm = 4.9028000661637961E3; % Moon gravitational parameter [km^3/s^2]
+mm = gmm/6.67384E-20; % Moon mass [kg]
+Rm = 1.7374000000000003E3; % Moon radius [km]
+
+mu = gmm/(gmE+gmm); % Mass ratio
+mstar = (gmE+gmm)/6.67384E-20; % Characteristic mass [kg]
+lstar = 3.8474799201129237E5; % Characteristic length [km]
+tstar = sqrt(lstar^3/(gmE+gmm)); % Characteristic time [s]
+
+%% Plot
+fig1 = figure("Position", [200 100 1200 750]);
+hold on
+% Earth = plot3DBody("Earth", RE/lstar, [-mu, 0, 0]);
+% set(Earth, 'DisplayName', "Earth")
+Moon = plot3DBody("Moon", Rm/lstar, [1-mu, 0, 0]);
+set(Moon, 'DisplayName', "Moon")
+scatter3(BCR4BPOrbit.x, BCR4BPOrbit.y, BCR4BPOrbit.z, 10*ones(length(BCR4BPOrbit.t), 1), angleColor(BCR4BPOrbit.theta4), 'filled', 'DisplayName', "BCR4BP 1:1 Syn.")
+plot3(CR3BPGuessOrbit.x, CR3BPGuessOrbit.y, CR3BPGuessOrbit.z, 'g', 'DisplayName', "CR3BP 1:1 Sid.")
+plot3(CR3BPCompOrbit.x, CR3BPCompOrbit.y, CR3BPCompOrbit.z, 'r', 'DisplayName', "CR3BP 1:1 Syn.")
+scatter3(BCR4BPOrbit2.x, BCR4BPOrbit2.y, BCR4BPOrbit2.z, 10*ones(length(BCR4BPOrbit2.t), 1), angleColor(BCR4BPOrbit2.theta4), 'filled', 'DisplayName', "BCR4BP 2:1 Syn.")
+plot3(CR3BPGuessOrbit2.x, CR3BPGuessOrbit2.y, CR3BPGuessOrbit2.z, 'g', 'DisplayName', "CR3BP 2:1 Sid.")
+plot3(CR3BPCompOrbit2.x, CR3BPCompOrbit2.y, CR3BPCompOrbit2.z, 'r', 'DisplayName', "CR3BP 2:1 Syn.")
+axis equal
+grid on
+xlabel("$x$ [EM ndim]", 'Interpreter', 'latex')
+ylabel("$y$ [EM ndim]", 'Interpreter', 'latex')
+zlabel("$z$ [EM ndim]", 'Interpreter', 'latex')
+title("Earth-Moon $L_{1}$ Lyapunovs", 'Interpreter', 'latex')
+legend('Location', 'bestoutside', 'Interpreter', 'latex');
+phasemap;
+phasebar('deg', 'Location', 'northeast', 'Size', 0.275)
+set(gca, 'Color', 'k');
+view(2)
+hold off
+% exportgraphics(fig1, 'PlotCR3BPTraj_1.png', 'BackgroundColor', 'k')
