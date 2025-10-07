9dd748a753fcd7e69c7362b3a76ee2ec0949153b
Jonathan Richmond <richmo11@purdue.edu>
2025-10-07 17:23:54 -0400

    Updated EscapeAnalysis script

--- EscapeAnalysis.m
+++ EscapeAnalysis.m
@@ -1,7 +1,7 @@
 %%% EscapeAnalysis.jl
 %%% Jonathan Richmond
 %%% C: 22 September 2025
-%%% U: 23 September 2025
+%%% U: 7 October 2025
 
 clear
 load('../PhDScripts/Output/EscapeAnalysis.mat')
@@ -20,7 +20,8 @@ mstar = (gmE+gmm)/6.67384E-20; % Characteristic mass [kg]
 lstar = 3.8474799201129237E5; % Characteristic length [km]
 tstar = sqrt(lstar^3/(gmE+gmm)); % Characteristic time [s]
 
-RH = 1.496557260502363E6; % Hills radius [km]
+% RH = 1.496557260502363E6; % Hills radius [km]
+RH = 600000;
 
 %% Sun-B1 Data
 gmS = 1.327124400419393E11; % Sun gravitational parameter [km^3/s^2]
@@ -140,7 +141,7 @@ ylim(ax3, [0 10])
 grid on
 ylabel("$e_{E}$", 'Interpreter', 'latex')
 hold off
-xlabel("t [EM ndim]", 'Interpreter', 'latex')
+xlabel("$t$ [EM ndim]", 'Interpreter', 'latex')
 linkaxes([ax1, ax2, ax3], 'x')
 leg3 = legend(ps);
 leg3.Layout.Tile = 'east';
@@ -165,15 +166,34 @@ hold off
 %% Sun-B1 Trajectory Plot
 fig5 = figure("Position", [200 100 1200 750]);
 hold on
+ax = gca;
+colors = ax.ColorOrder;
+nColors = size(colors, 1);
 % Sun = plot3DBody("Sun", RS/lstarSB1, [-muSB1, 0, 0]);
 % set(Sun, 'DisplayName', "Sun")
 B1 = plot3DBody("Earth", RE/lstarSB1, [1-muSB1, 0, 0]);
 set(B1, 'DisplayName', "Earth")
 SB1Trajs = [SB1Trajectory1, SB1Trajectory2, SB1Trajectory3, SB1Trajectory4, SB1Trajectory5, SB1Trajectory6, SB1Trajectory7];
 for j = 1:length(SB1Trajs)
-    plot3(SB1Trajs(j).x(1:end-10), SB1Trajs(j).y(1:end-10), SB1Trajs(j).z(1:end-10), 'DisplayName', "Escape Traj. "+num2str(j))
+    color = colors(mod(j-1, nColors)+1,:);
+    plot3(SB1Trajs(j).x(1:end-150), SB1Trajs(j).y(1:end-150), SB1Trajs(j).z(1:end-150), 'Color', color, 'DisplayName', "Escape Traj. "+num2str(j))
+    dot_SB1 = zeros(length(SB1Trajs(j).x(1:end-150)), 1);
+    for k = 1:length(dot_SB1)
+        dot_SB1(k) = dot([SB1Trajs(j).x(k)-1+muSB1, SB1Trajs(j).y(k) SB1Trajs(j).z(k)], [SB1Trajs(j).xdot(k), SB1Trajs(j).ydot(k), SB1Trajs(j).zdot(k)]);
+    end
+    apse = find((dot_SB1(1:end-1) > 0) & (dot_SB1(2:end) <= 0))+1;
+    for k = 1:length(apse)
+        scatter3(SB1Trajs(j).x(apse(k)), SB1Trajs(j).y(apse(k)), SB1Trajs(j).z(apse(k)), 50, 'w', '*', 'HandleVisibility', 'off')
+    end
 end
-plot3(RH/lstarSB1*cos(linspace(0, 2*pi, 101))+1-muSB1, RH/lstarSB1*sin(linspace(0, 2*pi, 101)), zeros(1, 101), 'w:', 'DisplayName', "Hills Radius")
+% plot3(RH/lstarSB1*cos(linspace(0, 2*pi, 101))+1-muSB1, RH/lstarSB1*sin(linspace(0, 2*pi, 101)), zeros(1, 101), 'w:', 'DisplayName', "Hills Radius")
+plot3(RH/lstarSB1*cos(linspace(0, 2*pi, 101))+1-muSB1, RH/lstarSB1*sin(linspace(0, 2*pi, 101)), zeros(1, 101), 'w:', 'DisplayName', "Escape Radius")
+for j = 1:length(L1StableMan.arcs)
+    plot3(L1StableMan.arcs{j}.x, L1StableMan.arcs{j}.y, L1StableMan.arcs{j}.z, 'b', 'LineWidth', 0.5, 'HandleVisibility', 'off')
+end
+% for j = 1:length(L2StableMan.arcs)
+%     plot3(L2StableMan.arcs{j}.x, L2StableMan.arcs{j}.y, L2StableMan.arcs{j}.z, 'c', 'LineWidth', 0.5, 'HandleVisibility', 'off')
+% end
 axis equal
 grid on
 xlabel("$x$ [S$B_{1}$ ndim]", 'Interpreter', 'latex')
@@ -193,8 +213,18 @@ t6.TileSpacing = 'compact';
 t6.Padding = 'compact';
 ax1 = nexttile;
 hold on
-colors = ax1.ColorOrder;
-nColors = size(colors, 1);
+ps = gobjects(size(t, 1), 1);
+for j = 1:size(t, 1)
+    color = colors(mod(j-1, nColors)+1,:);
+    ps(j) = plot(SB1Trajs(j).t, SB1Trajs(j).H, 'Color', color, 'DisplayName', "$v_{E}="+num2str(v_esc(j))+"$");
+end
+yline(H_E2, 'w:', 'LineWidth', 1, 'HandleVisibility', 'off')
+grid on
+ylabel("$H_{SB_{1}}$ [S$B_{1}$ ndim]", 'Interpreter', 'latex')
+title("Sun-$B_{1}$ Hamiltonian", 'Interpreter', 'latex')
+hold off
+ax2 = nexttile;
+hold on
 for j = 1:size(t, 1)
     change = [1; find(diff(quad_SB1{j}) ~= 0)+1; length(quad_SB1{j})];
     color = colors(mod(j-1, nColors)+1,:);
@@ -202,31 +232,64 @@ for j = 1:size(t, 1)
         range = change(k):change(k+1);
         quad = quad_SB1{j}(range(1));
         if (quad == 1) || (quad == 3)
-            plot(t{j}(range), H_SB1{j}(range), 'Color', color, 'HandleVisibility', 'off')
+            plot(SB1Trajs(j).t(range), e_osc_E{j}(range), 'Color', color, 'HandleVisibility', 'off')
         else
-            plot(t{j}(range), H_SB1{j}(range), 'LineStyle', '--', 'Color', color, 'HandleVisibility', 'off')
+            plot(SB1Trajs(j).t(range), e_osc_E{j}(range), 'LineStyle', ':', 'Color', color, 'HandleVisibility', 'off')
         end
     end
-end
-yline(H_E1, 'w:', 'LineWidth', 1, 'HandleVisibility', 'off')
-grid on
-ylabel("$H_{SB_{1}}$ [S$B_{1}$ ndim]", 'Interpreter', 'latex')
-title("Sun-$B_{1}$ Hamiltonian", 'Interpreter', 'latex')
-hold off
-ax2 = nexttile;
-hold on
-ps = gobjects(size(t, 1), 1);
-for j = 1:size(t, 1)
-    ps(j) = plot(t{j}, e_osc_E{j}, 'DisplayName', "$v_{E}="+num2str(v_esc(j))+"$");
+    dot_SB1 = zeros(length(SB1Trajs(j).x(1:end-10)), 1);
+    for k = 1:length(dot_SB1)
+        dot_SB1(k) = dot([SB1Trajs(j).x(k)-1+muSB1, SB1Trajs(j).y(k) SB1Trajs(j).z(k)], [SB1Trajs(j).xdot(k), SB1Trajs(j).ydot(k), SB1Trajs(j).zdot(k)]);
+    end
+    apse = find((dot_SB1(1:end-1) > 0) & (dot_SB1(2:end) <= 0))+1;
+    for k = 1:length(apse)
+        scatter(SB1Trajs(j).t(apse(k)), e_osc_E{j}(apse(k)), 50, 'w', '*', 'HandleVisibility', 'off')
+    end
 end
 yline(1, 'w:', 'LineWidth', 1, 'HandleVisibility', 'off')
 ylim(ax2, [0 10])
 grid on
 ylabel("$e_{E}$", 'Interpreter', 'latex')
 hold off
-xlabel("t [EM ndim]", 'Interpreter', 'latex')
+xlabel("$t$ [S$B_{1}$ ndim]", 'Interpreter', 'latex')
 linkaxes([ax1, ax2], 'x')
 leg6 = legend(ps);
 leg6.Layout.Tile = 'east';
 leg6.Interpreter = 'latex';
 % exportgraphics(fig6, 'EscapeAnalysis_6.png', 'BackgroundColor', 'k')
+
+%% Jacobi Constants
+fig7 = figure("Position", [200 100 1200 750]);
+t7 = tiledlayout(2,1);
+t7.TileSpacing = 'compact';
+t7.Padding = 'compact';
+ax1 = nexttile;
+hold on
+ps = gobjects(size(t, 1), 1);
+for j = 1:size(t, 1)
+    color = colors(mod(j-1, nColors)+1,:);
+    ps(j) = plot(t{j}, JC_EM{j}, 'Color', color, 'DisplayName', "$h_{z}="+num2str(h_z(j))+"$");
+end
+yline(JC_EM_L2, 'w:', 'LineWidth', 1, 'HandleVisibility', 'off')
+ylim(ax1, [0 4])
+grid on
+ylabel("JC [EM ndim]", 'Interpreter', 'latex')
+title("Jacobi Constants", 'Interpreter', 'latex')
+hold off
+ax2 = nexttile;
+hold on
+for j = 1:size(t, 1)
+    color = colors(mod(j-1, nColors)+1,:);
+    plot(t{j}, JC_SB1{j}, 'Color', color, 'HandleVisibility', 'off')
+end
+yline(JC_SB1_L2, 'w:', 'LineWidth', 1, 'HandleVisibility', 'off')
+ylim(ax2, [3.0 3.003])
+grid on
+ylabel("JC [S$B_{1}$ ndim]", 'Interpreter', 'latex')
+hold off
+xlabel("$t$ [EM ndim]", 'Interpreter', 'latex')
+linkaxes([ax1, ax2], 'x')
+leg7 = legend(ps);
+leg7.Layout.Tile = 'east';
+leg7.Interpreter = 'latex';
+% exportgraphics(fig7, 'EscapeAnalysis_7.png', 'BackgroundColor', 'k')
